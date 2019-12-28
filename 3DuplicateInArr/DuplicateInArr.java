ac0e8f8e3a86b8993a574a53237c9a9825c4a931
MVPGFC <mvpgfc@live.com>
2019-12-28 23:16:49 +0800

    第3题 数组中重复数据

--- 3DuplicateInArr/DuplicateInArr.java
+++ 3DuplicateInArr/DuplicateInArr.java
@@ -2,7 +2,7 @@ import com.sun.javaws.jnl.LDUpdater;
 
 public class DuplicateInArr {
 
-    public boolean duplicate(int numbers[], int length, int[] duplication) {
+    private static boolean duplicate(int[] numbers, int length, int[] duplication) {
         if (length <= 1 || numbers == null)
             return false;
 
@@ -28,12 +28,11 @@ public class DuplicateInArr {
     }
 
     public static void main(String[] args) {
-        DuplicateInArr duplicateInArr = new DuplicateInArr();
         int[] arr = {2,3,1,0,2,5,3};
         int[] arr2 = {1,2,3,4,5,6,6};
         int length = arr.length;
         int[] res = new int[1];
-        boolean flag = duplicateInArr.duplicate(arr2, length, res);
+        boolean flag = duplicate(arr2, length, res);
         System.out.println(flag);
         System.out.println(res[0]);
     }
