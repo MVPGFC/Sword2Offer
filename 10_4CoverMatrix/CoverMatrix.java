2d0eb879261fd18c027fc957516e91a7e5d4d4c8
MVPGFC <mvpgfc@live.com>
2020-01-06 20:56:00 +0800

    覆盖矩阵问题，和只能一步或者两步跨台阶问题一样

--- 10_4CoverMatrix/CoverMatrix.java
+++ 10_4CoverMatrix/CoverMatrix.java
@@ -1,2 +1,23 @@
 public class CoverMatrix {
+    public int RectCover(int target) {
+        if (target < 1)
+            return 0;
+
+        int[] res = {1, 2};
+        int way1 = 1;
+        int way2 = 2;
+        int wayRes = 0;
+
+        if (target < 3)
+            return res[target - 1];
+
+        for (int i = 3; i <= target; i++) {
+            wayRes = way1 + way2;
+
+            way1 = way2;
+            way2 = wayRes;
+        }
+
+        return wayRes;
+    }
 }
